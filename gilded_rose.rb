9bee49fff4315e309a137710687c5961e6a3a86b
Ahsan Ellahi <mail.ahsanellahi@gmail.com>
2019-07-02 04:02:53 +0500

    GildedRose: Refactored code

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,55 +1,67 @@
 class GildedRose
+  QUALITY_UPPER_LIMIT = 50
+  QUALITY_LOWER_LIMIT = 0
+
+  attr_accessor :items, :item, :index, :factors
+
   def initialize(items)
     @items = items
+    @factors = Hash.new(1)
   end
 
-  def update_quality()
-    @items.each do |item|
-      if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
-        if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
+  def update_quality
+    items.each_with_index do |item, index|
+      @item = item
+      @index = index
+
+      set_item_sell_in
+
+      case item.name
+        when 'Aged Brie' then update_aged_brie_quality
+        when 'Backstage passes to a TAFKAL8QUALITY_LOWER_LIMITETC concert' then update_backstage_passes_quality
+        when 'Conjured Mana Cake' then update_conjured_quality
+        else update_others_quality
       end
-      if item.name != 'Sulfuras, Hand of Ragnaros'
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != 'Aged Brie'
-          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            if item.quality > 0
-              if item.name != 'Sulfuras, Hand of Ragnaros'
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+
+      set_item_quality_with_constraint
     end
   end
+
+  private
+    def update_aged_brie_quality
+      item.quality = item.quality.succ if item.quality <= QUALITY_UPPER_LIMIT && item.quality > QUALITY_LOWER_LIMIT
+    end
+
+    def set_item_sell_in
+      item.sell_in = item.sell_in - 1 unless item.name == 'Sulfuras, Hand of Ragnaros'
+    end
+
+    def update_backstage_passes_quality
+      if item.quality <= QUALITY_UPPER_LIMIT && item.quality > QUALITY_LOWER_LIMIT
+        item.quality = item.quality.succ
+        item.quality = item.quality.succ if item.sell_in < 11 && item.sell_in > 5
+        item.quality = item.quality + 2 if item.sell_in < 6
+      end
+    end
+
+    def update_others_quality
+      if item.quality <= QUALITY_UPPER_LIMIT && item.quality > QUALITY_LOWER_LIMIT
+        item.quality = item.quality - factors[index]
+        factors[index] = factors[index] * 2
+      end
+    end
+
+    def update_conjured_quality
+      if item.quality <= QUALITY_UPPER_LIMIT && item.quality > QUALITY_LOWER_LIMIT
+        item.quality = item.quality - factors[index]
+        factors[index] = factors[index] * 4
+      end
+    end
+
+    def set_item_quality_with_constraint
+      item.quality = QUALITY_LOWER_LIMIT unless item.quality > QUALITY_LOWER_LIMIT
+      item.quality = QUALITY_UPPER_LIMIT if item.quality >= QUALITY_UPPER_LIMIT
+    end
 end
 
 class Item
